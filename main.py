c030adcd511f36fbe768420c441451782786f89d
AppleMonkey <monkeyappleh@gmail.com>
2021-05-02 20:53:52 +0800

    [*]searching functions

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 from copy import deepcopy
+import json
 class Solution:
-    def permute(self,word):
-        permutations=[]
-        self.generatePermutations([],word,permutations)
-        return permutations
-    def generatePermutations(self,running_choices,originalStr,permutations):
-        if len(running_choices)==len(originalStr):
-            permutations.append(deepcopy(running_choices))
-            return
-        for i in range(0,len(originalStr)):
-            choice =originalStr[i]
-            if choice in running_choices:
-                continue
-            running_choices.append(choice)
-            self.generatePermutations(running_choices,originalStr,permutations)
-            running_choices.pop()
+    def __init__(self):
+        with open('cleanedDict.json') as json_file:
+            self.dictRef=json.load(json_file)
+    
+#Step1: Got combinations from given words
 
     def letterCombinations(self):
         self.refList=['bona','exy','seola']
         combination=[]
         if self.refList==None:
             return None
-
         self.helper("",0,combination)
         return combination
     def helper(self,choices,currentIdx,combinations):
@@ -33,10 +23,42 @@ class Solution:
             self.helper(choices,currentIdx+1,combinations)
             choices=choices[:-1]
 
-    def referenceList(self,inputList):
-        pass
+#Step2:Got permutations of each combination
+ 
+    def permute(self,word):
+        permutations=set()
+        self.generatePermutations([],word,permutations)
+        return permutations
+    def generatePermutations(self,running_choices,originalStr,permutations):
+        if len(running_choices)==len(originalStr):
+            permuStr=(''.join(running_choices))
+            permutations.add(permuStr)
+            # permutations.add(deepcopy(running_choices))
+            return
+        for i in range(0,len(originalStr)):
+            choice =originalStr[i]
+            if choice in running_choices:
+                continue
+            running_choices.append(choice)
+            self.generatePermutations(running_choices,originalStr,permutations)
+            running_choices.pop()
 
+#Step3:Search in dictionary
+    def searchInDict(self,permutations,dictRef):
+        result=[]
+        for i in permutations:
+            if i in dictRef:
+                result.append(i)
+        return result
 
 c=Solution()
+combinations=list(set(c.letterCombinations()))
 
-print(c.letterCombinations())
+result=[]
+for i in combinations:
+    permutationResult=list(c.permute(i))
+    result.extend(permutationResult)
+result=list(set(result))
+
+print(c.searchInDict(result,c.dictRef))
+
