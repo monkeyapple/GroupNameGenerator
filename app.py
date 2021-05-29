1631ed8c04de224847afc9d895bc121804cdea9d
monkeyapple <50127732+monkeyapple@users.noreply.github.com>
2021-05-29 17:38:14 +0800

    form bootstrap

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from wtforms.validators import Length
 from copy import deepcopy
 from algorithm import Solution
 import json
+from flask_bootstrap import Bootstrap
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='mysecretkey'
+Bootstrap(app)
 
 ###################################################
 ##################### Form #######################
@@ -27,6 +29,7 @@ class InputForm(FlaskForm):
 ###################################################
 ###################### View #######################
 ###################################################
+
 @app.route('/',methods=['GET','POST'])
 def index():
     form=InputForm()
@@ -74,6 +77,7 @@ def index():
 @app.route('/result')
 def result():
     return render_template('result.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,21 +1,11 @@
-from flask import Flask, session,render_template,redirect,url_for
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 
 app=Flask(__name__)
-app.config['SECRET_KEY']='mysecretkey'
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('test.html', form=form)
+Bootstrap(app)
 
+@app.route('/')
+def index():
+    return render_template('test.html')
 if __name__ == '__main__':
     app.run(debug=True)
